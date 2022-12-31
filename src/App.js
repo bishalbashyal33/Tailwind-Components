c4e797651571416e2d6828cf8e3175c82ebd4b11
Bishal Bashyal <waglemanish97@gmail.com>
2022-12-31 19:17:39 +0545

    Tailwind Flowbite Homepage Completed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import MyButton from "./components/button";
 import Navbar from "./components/navbar";
+import HeroSection from "./components/herosection";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link
 } from 'react-router-dom';
+import Footer from "./components/footer";
+
 
 export default function App() {
   return (
-    <>
-      <Router>
-       <Navbar/>
-      
-       <Routes>
-     
-       {/* <Route path='/get-started' element={<GetStarted/>} />
-       <Route path='/upload-page' element={<UploadPageSection/>} />
-       <Route path='/' element={<Home/>} />
-       <Route path='/extraction-status' element={<ExtractionStatus/>} />
-       <Route path='/signup-page' element={<SignupPageSection/>} />
-       <Route path='/login-page' element={<LoginPageSection/>} /> */}
-
-            
-       </Routes>
-      </Router>
-     </>
+   <>
+  
+   <Navbar />
+   <HeroSection/>
+   <Footer/>
+   </>
   );
 }
