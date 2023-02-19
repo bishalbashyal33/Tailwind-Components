beb707012a34669b81f220dc18db6d166c846b6e
Bishal Bashyal <waglemanish97@gmail.com>
2023-02-19 21:58:05 +0545

    sidebar tabs static implementation done

--- src/components/sidebar/apiservices.js
+++ src/components/sidebar/apiservices.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import DocType from '../dashboardcomponents/doctype';
+import SerType from '../dashboardcomponents/servicetype';
 
 function APIService(props) {
 
@@ -8,9 +9,11 @@ function APIService(props) {
     
 
     <div class="p-4 sm:ml-64">
-    <div class="p-4 border-2 text-white border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-    "This is an API tab"
+    <div class="p-4 flex overflow-auto scrollbar-hide  border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
      
+        <SerType img="https://upload.wikimedia.org/wikipedia/commons/d/da/Google_Drive_logo.png" apiname="Google Drive" />
+        <SerType img="https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_512px.png" apiname="Mail Server" />
+        <SerType img="https://styles.redditmedia.com/t5_22y58b/styles/communityIcon_r5ax236rfw961.png" apiname="Fast API" />
     </div>
   </div>
 
