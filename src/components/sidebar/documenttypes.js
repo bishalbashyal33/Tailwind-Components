42dfd5c55a0375bdc55942656684becde79d49f0
Bishal Bashyal <waglemanish97@gmail.com>
2023-03-14 08:12:10 +0545

    debugged buggy UI of documenttype tab using flex and overflow management

--- src/components/sidebar/documenttypes.js
+++ src/components/sidebar/documenttypes.js
@@ -8,10 +8,13 @@ function DocumentTypes(props) {
   return (
     
 
-    <div class="p-4 sm:ml-64">
-    <div class="flex p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-     
+    <div class="p-4 h-screen overflow-y-scroll overflow-x-clip scrollbar-hide sm:ml-64">
+    <div class="flex flex-wrap p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+   
         <DocType />
+       
+        <FormType/>
+        
         <FormType/>
      
     </div>
