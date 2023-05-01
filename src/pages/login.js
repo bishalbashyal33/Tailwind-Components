ac4182c5bd88eff85ec984c5d7ba70d32b8a9b7a
aayushshah.kanu <aayushshah.kanu@docsumo.com>
2023-05-01 21:26:39 +0545

    Updated placeholder for forms

--- src/pages/login.js
+++ src/pages/login.js
@@ -50,7 +50,7 @@ function LogIn () {
                         onChange={handleEmailChange}
                         id="email"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="name@flowbite.com"
+                        placeholder="user@email.com"
                         required
                     />
                 </div>
